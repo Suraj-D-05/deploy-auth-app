b5fd3980074dd0126a0d51d05cc7dc7b88e95342
Suraj-D-05 <surajdemanna05@gmail.com>
2024-09-19 19:47:09 +0530

    Update SignUp.js

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -30,7 +30,7 @@ const SignUp = () => {
       return handleError("name, email and password are required !")
     }
     try{
-      const url = 'http://localhost:8080/auth/signup';
+      const url = 'https://deploy-auth-app-api.vercel.app/auth/signup';
       const response = await fetch(url, {
         method: "POST",
         headers: {
